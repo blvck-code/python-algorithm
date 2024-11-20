ae2edaea7671809239ec8f20365b6d2a280ff137
blvck <brianmauriceoluoch@gmail.com>
2024-11-20 15:08:39 +0300

    latest on functions

--- funct.py
+++ funct.py
@@ -48,21 +48,100 @@ def func(spam, eggs, toast=0, ham=0):
 func(1, 2)
 func(1, ham=1, eggs=2)
 
-def f(*args):
+# * and ** are designed to support functions that take any number of arguments
+
+def f(*args): # python collects all the arguments into a new tuple & assigns the variable args to the tuple
     print(args)
 
 f(1, 2, 3)
 
-def f(**args):
+def f(**args): # ** form allows you to convert from keywords to dictionaries
     print(args)
 
 f(a=1, b=2)
-f(a=1, b=2)
+f(a=1, b=20)
 
 
 def f(a, *pargs, **kargs): # 1 is passed to a by position, 2 & 3 into pargs positional tuple and x & y into kargs by dict
     print(a, pargs, kargs)
 
 f(1, 2, 3, x=1, y=2)
+
+def func(name='Bob', age=40, job='dev'):
+    print(name)
+
+func()
+
+# Unpacking arguments
+def func(a, b, c, d):
+    print('a =>', a, 'b =>', b,'c =>', c,'d =>', d)
+
+args = (1, 2)
+args += (3, 4)
+func(*args)
+
+args = {'a': 1, 'b': 2, 'c': 3}
+args['d'] = 5
+func(**args)
+
+# The complex section
+func(*(1, 2), **{'d':4, 'c': 60})
+func(1, *(2, 3), **{ 'd': 7 })
+func(1, c=3, *(2,), **{'d': 10} )
+func(1, *(2, 3), d=4)
+func(1, *(2, ), c=3, **{'d': 4})
+
+
+def kwonly(a, *b, c):
+    print(a, b, c)
+
+kwonly(1, 2, c=3) # b collects any extra positional arguments & c must be passed by keyword only
+kwonly(1, c=3)
+
+def kwonly(a, *, b, c): # * is to indicate the function does not accept-length argument list but still expects all arguments
+    print(a, b, c)      #following the * to be passed as keywords
+
+kwonly(1, c=3, b=2)
+kwonly(a=1, c=3, b=2)
+
+def kwonly(a, *, b='spam', c='ham'):       # a may be passed by name or position, and b and c are optional but must be passed if keyword used
+    print(a, b, c)
+
+kwonly(1)
+kwonly(1, c=3)
+kwonly(a=1)
+# kwonly(1, 2)
+
+
+
+def kwonly(a, *, b, c='ham'): # keyword-only arguments with defaults are optional, but those without defaults
+    print(a, b, c)                   # effectively become required keywords for the function
+
+kwonly(1, b='eggs')
+# kwonly(1, c='eggs') fails cos b keyword is needed
+
+def kwonly(a, *, b=1, c, d=2):
+    print(a, b, c, d)
+
+kwonly(4, c=4) # a and c keyword must be provided
+
+
+# Ordering rules
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
